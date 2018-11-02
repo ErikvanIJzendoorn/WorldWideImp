9ccf94405b9ae81fe92e8509efdee216afba2501
ErikvanIJzendoorn <erik.vanijzendoorn@hotmail.com>
2018-11-02 08:43:18 +0100

    Add back-end and db connection

---
+++ db/connect.php
@@ -0,0 +1,26 @@
+<?php 
+
+// Get a single product from the DB
+function getProduct() {
+
+	$db ="mysql:host=localhost;dbname=wideworldimporters;port=3306";
+	$user = "root";
+	$pass = "";
+	$pdo = new PDO($db, $user, $pass);
+
+	$stmt = $pdo->prepare("
+		SELECT StockItemName naam, SG.StockGroupName categorie, CustomFields herkomst, PackageTypeName verpakking, ColorName kleur, UnitPrice prijs, TaxRate btw, QuantityOnHand voorraad, S.StockItemID itemID, SG.StockGroupID categorieID, C.ColorID kleurID, P.PackageTypeID verpakkingID
+			FROM StockItems S
+			LEFT JOIN Colors C ON S.ColorID = C.ColorID
+			LEFT JOIN PackageTypes P ON S.UnitPackageID = P.PackageTypeID
+			LEFT JOIN StockItemHoldings H ON S.StockItemID = H.StockItemID
+			LEFT JOIN StockItemStockGroups SS ON S.StockItemID = SS.StockItemID
+			LEFT JOIN StockGroups SG ON SS.StockGroupID = SG.StockGroupID
+			WHERE S.StockItemID = 1 AND SG.StockGroupID = 6
+	");
+
+	$stmt->execute();
+	return $stmt;
+}
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,47 @@
 <?php 
 
-header("Location: landing/index.php");
+require "db/connect.php";
+//header("Location: landing/index.php");
+
+$stmt = getProduct();
+
+while($row = $stmt->fetch()) {
+	$naam = $row['naam'];
+	$categorie = $row['categorie'];
+	$herkomst = $row['herkomst'];
+	$verpakking = $row['verpakking'];
+	$kleur = $row['kleur'];
+	$prijs = $row['prijs'];
+	$btw = $row['btw'];
+	$voorraad = $row['voorraad'];
+	$ItemID = $row['itemID'];
+	$categorieID = $row['categorieID'];
+	$kleurID = $row['kleurID'];
+	$verpakkingID = $row['verpakkingID'];
+}
+$herkomst = str_replace(array('"', "{", "}", "[", "]", "-", ":", ","), "", $herkomst);
+$herkomst = explode(" ", $herkomst);
+
+print($naam . "<br>");
+print($categorie . "<br>");
+print($herkomst[2] . "<br>");
+print($verpakking . "<br>");
+if(isset($kleur)) {
+	print($kleur . "<br>");
+} else {
+	print("Geen kleur meegegeven") . "<br>";
+}
+
+print("€ $prijs verkoopprijs <br>");
+print("€ $btw btw <br>");
+print($voorraad . "<br>");
+print($ItemID . "<br>");
+print($categorieID . "<br>");
+if(isset($kleurID)) {
+	print($kleurID . "<br>");
+} else {
+	print("Geen kleur ID meegegeven" . "<br>");
+}
+print($verpakkingID . "<br>");
 
 ?>
