11b14c1e6d2d6c939d6ccc71941be84ce992d1c1
ErikvanIJzendoorn <erik.vanijzendoorn@hotmail.com>
2018-11-02 10:53:38 +0100

    Add landing page

--- db/connect.php
+++ db/connect.php
@@ -10,17 +10,34 @@ function getProduct() {
 
 	$stmt = $pdo->prepare("
 		SELECT StockItemName naam, SG.StockGroupName categorie, CustomFields herkomst, PackageTypeName verpakking, ColorName kleur, UnitPrice prijs, TaxRate btw, QuantityOnHand voorraad, S.StockItemID itemID, SG.StockGroupID categorieID, C.ColorID kleurID, P.PackageTypeID verpakkingID
-			FROM StockItems S
-			LEFT JOIN Colors C ON S.ColorID = C.ColorID
-			LEFT JOIN PackageTypes P ON S.UnitPackageID = P.PackageTypeID
-			LEFT JOIN StockItemHoldings H ON S.StockItemID = H.StockItemID
-			LEFT JOIN StockItemStockGroups SS ON S.StockItemID = SS.StockItemID
-			LEFT JOIN StockGroups SG ON SS.StockGroupID = SG.StockGroupID
-			WHERE S.StockItemID = 1 AND SG.StockGroupID = 6
+		FROM StockItems S
+		LEFT JOIN Colors C ON S.ColorID = C.ColorID
+		LEFT JOIN PackageTypes P ON S.UnitPackageID = P.PackageTypeID
+		LEFT JOIN StockItemHoldings H ON S.StockItemID = H.StockItemID
+		LEFT JOIN StockItemStockGroups SS ON S.StockItemID = SS.StockItemID
+		LEFT JOIN StockGroups SG ON SS.StockGroupID = SG.StockGroupID
+		WHERE S.StockItemID = 1 AND SG.StockGroupID = 6
 	");
 
 	$stmt->execute();
 	return $stmt;
 }
+
+function getCategory(){
+	$db ="mysql:host=localhost;dbname=wideworldimporters;port=3306";
+	$user = "root";
+	$pass = "";
+	$pdo = new PDO($db, $user, $pass);
+
+	$stmt = $pdo->prepare("
+		SELECT SG.StockGroupName naam, S.photo foto, SG.StockGroupID id
+		FROM StockGroups SG
+		JOIN stockitemstockgroups USING(StockGroupID)
+		JOIN StockItems S USING(StockItemID)
+		GROUP BY SG.StockGroupID");
+
+		$stmt->execute();
+		return $stmt;
+	}
 
 ?>
--- landing/index.php
+++ landing/index.php
@@ -1,9 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>World Wide Importers</title>
+	<link rel="stylesheet" href="landing.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<?php require "../db/connect.php" ?>
 </head>
 <body>
+<nav class="navbar navbar-expand navbar-dark bg-dark sticky-top">
+  <div class="navbar-collapse">
+    <ul class="navbar-nav mr-auto">
+		<li class="nav-item active">
+			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		</li>
+    </ul>
+    <ul class="navbar-nav mr-auto">
+    	<li class="nav-item active">
+			<a class="navbar-brand" href="#">WorldWideImporters</a>
+		</li>
+    </ul>
 
-</body>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    <div class="container">
+    	<div class="row">
+			<?php 
+				$allCat = [];
+				$stmt = getCategory();
+				while($row = $stmt->fetch()) {
+					$id = $row['id'];
+					$naam = $row['naam'];
+					$foto = $row['foto'];
+					$cat = array($id, $naam, $foto);
+			?>
+				<div class="col-md-3 offset-1 cat_item">
+					<img src="https://via.placeholder.com/150" alt="test"><br>
+					<div style="text-align: center; max-width: 150px;">
+						<span><?php print($cat[1]); ?></span>
+					</div>
+				</div>
+			<?php
+				}
+			?>
+		</div>
+    </div>
+
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
+  </body>
 </html>
