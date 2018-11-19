d36563c8ece2eac78c714891f15d97dc9a312b32
SonjaVredeveld <sonjavredeveld@gmail.com>
2018-11-19 20:10:21 +0100

    New Landing Header

--- landing/index.php
+++ landing/index.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 		<!--header&navbar-->
-	<?php require "../main/header.php"; ?>
+	<?php require "../main/landingheader.php"; ?>
 	<!-- meta -->
 	<?php require"../main/meta.html"; ?>
 	<!-- database -->
