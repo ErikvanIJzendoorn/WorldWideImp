b055eed6c9b6ca1e763395569d3c94b3b2941754
stfnkts <stefan221100@gmail.com>
2018-11-23 15:59:25 +0100

    Update index.php

--- winkelwagen/index.php
+++ winkelwagen/index.php
@@ -64,8 +64,8 @@
                                             <div class="media">
                                                 <a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a>
                                                 <div class="media-body">
-                                                <h4 class="media-heading"><a href="#"><?=$naam;?></a></h4>
-                                                <h5 class="media-heading"> category: <a href="#"><?=$category?></a></h5>
+                                                <h4 class="media-heading"> <a href="../product/index.php?product=<?=$id?>"><?=$naam;?></a></h4>
+                                                <h5 class="media-heading"> category: <a href="../overzicht/productpage.php?category=<?=$id?>&pageNumber=0"><?=$category?></a></h5>
                                                 </div>
                                             </div>
                                         </td>
