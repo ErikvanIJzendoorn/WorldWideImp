1289a8ee48bc5e50944fced691509120996ab245
tessspr <tspoor2000@gmail.com>
2018-11-26 16:28:34 +0100

    Image Slide Show

--- product/index.php
+++ product/index.php
@@ -2,8 +2,6 @@
 <html>
 <head>
 <title>Productpagina</title>
-
-
     <!--header&navbar-->
     <?php require "../main/header.php"; ?>
     <?php require "../main/nav.php"; ?>
@@ -20,9 +18,46 @@
         <!-- Productnaam -->
         <?=$naam; ?>
         </p>
+
+</div>
+    
+<div class="outer-slide-container">
+  <div id="myCarousel" class="carousel slide slider-container" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="https://picsum.photos/400/400
+" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="https://picsum.photos/400/400" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="https://picsum.photos/400/400" alt="New york" style="width:100%;">
+      </div>
     </div>
-    <div class="picture">
-       <img src="https://via.placeholder.com/397" alt="Productimg">
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
     </div>
     <div class="product-description">
         <form action="cart.php" method="get">
