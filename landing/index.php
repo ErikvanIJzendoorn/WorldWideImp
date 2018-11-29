d1ba1b985c49da94327ec3a792263e18fc6e97a4
Unknown <jjtiessen@hotmail.com>
2018-11-29 15:00:57 +0100

    Add image to landing page.

--- landing/index.php
+++ landing/index.php
@@ -22,11 +22,41 @@
 					$id = $row['id'];
 					$naam = $row['naam'];
 					$cat = array($id, $naam);
+                                        
+                                        switch ($id) {
+                                            case 1:
+                                                $productimg = "../img/products/Novelty3.jpg";
+                                                break;
+                                            case 2:
+                                                $productimg = "../img/products/Hoodie1.jpg";
+                                                break;
+                                            case 3:
+                                                $productimg = "../img/products/Mug1.jpg";
+                                                break;
+                                            case 4:
+                                                $productimg = "../img/products/T-shirt1.jpg";
+                                                break;
+                                            case 6:
+                                                $productimg = "../img/products/Novelty1.jpg";
+                                                break;
+                                            case 7:
+                                                $productimg = "../img/products/Usb2.jpg";
+                                                break;
+                                            case 8:
+                                                $productimg = "../img/products/Slippers2.jpg";
+                                                break;
+                                            case 9:
+                                                $productimg = "../img/products/Toy3.jpg";
+                                                break;
+                                            case 10:
+                                                $productimg = "../img/products/Materials1.jpg";
+                                                break;
+                                        }
                                 
 			?>
 				<div class="col-md-3 offset-1 cat_item">
 					<a href="../overzicht/productpage.php?category=<?php echo $id;?>&pageNumber=0&sort=0&productAmount=30&filter=0&filterValue=10" style="color: black; text-decoration: none;">
-						<img src="https://via.placeholder.com/150" alt="test"><br>
+						<img src="<?=$productimg;?>" height="150" widt="150" alt="test"><br>
 						<div style="text-align: center; max-width: 150px;">
 							<span><?php print($cat[1]); ?></span>
 						</div>
