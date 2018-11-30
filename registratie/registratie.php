829bad5f3ff8f1560dc55b31bfd5473af81fe2bc
ErikvanIJzendoorn <erik.vanijzendoorn@hotmail.com>
2018-11-30 13:33:18 +0100

    Revert "Class change in autocomplete="off""
    This reverts commit 1e6d6180325c3b79fed20d0dae841ce98f810833.

--- registratie/registratie.php
+++ registratie/registratie.php
@@ -28,14 +28,14 @@
             <div class="row">
               <div class="col-md-6 mb-3">
                 <label for="firstName">First name</label>
-                <input type="text" autocomplete="off" name="firstName" placeholder="" value="" required>
+                <input type="text" class="form-control" name="firstName" placeholder="" value="" required>
                 <div class="invalid-feedback">
                   Valid first name is required.
                 </div>
               </div>
               <div class="col-md-6 mb-3">
                 <label for="lastName">Last name</label>
-                <input type="text" autocomplete="off" name="lastName" placeholder="" value="" required>
+                <input type="text" class="form-control" name="lastName" placeholder="" value="" required>
                 <div class="invalid-feedback">
                   Valid last name is required.
                 </div>
@@ -44,7 +44,7 @@
 
             <div class="mb-3">
               <label for="email">Email ( used for login )</label>
-              <input type="email" autocomplete="off" name="email" placeholder="you@example.com" required>
+              <input type="email" class="form-control" name="email" placeholder="you@example.com" required>
               <div class="invalid-feedback">
                 Please enter a valid email address for shipping updates.
               </div>
@@ -52,27 +52,27 @@
 
             <div class="mb-3">
               <label for="pass">Password</label>
-              <input type="password" autocomplete="off" name="pass" placeholder="abc123" required>
+              <input type="password" class="form-control" name="pass" placeholder="abc123" required>
             </div>
 
             <div class="row">
               <div class="col-md-6 mb-6">
               <label for="address">Address</label>
-              <input type="text" autocomplete="off" name="address" placeholder="1234 Main St" required>
+              <input type="text" class="form-control" name="address" placeholder="1234 Main St" required>
               <div class="invalid-feedback">
                 Please enter your address.
               </div>
             </div>
               <div class="col-md-3 mb-3">
                 <label for="zip">Zip</label>
-                <input type="text" autocomplete="off" name="zip" placeholder="" required>
+                <input type="text" class="form-control" name="zip" placeholder="" required>
                 <div class="invalid-feedback">
                   Zip code required.
                 </div>
               </div>
               <div class="col-md-3 mb-3">
                 <label for="zip">City</label>
-                <input type="text" autocomplete="off" name="city" placeholder="" required>
+                <input type="text" class="form-control" name="city" placeholder="" required>
                 <div class="invalid-feedback">
                   City required.
                 </div>
@@ -80,7 +80,7 @@
             </div>
             <hr class="mb-4">
             <div class="custom-control custom-checkbox">
-              <input type="checkbox" autocomplete="off" id="eula" required>
+              <input type="checkbox" class="custom-control-input" id="eula" required>
               <label class="custom-control-label" for="eula">I agree with the terms of service</label>
             </div>
             <hr class="mb-4">
@@ -90,7 +90,7 @@
             <div class="row">
               <div class="col-md-6 my-3">
               <div class="custom-control custom-radio">
-                <input id="credit" name="paymentMethod" type="radio" autocomplete="off" value="ideal" checked required>
+                <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" value="ideal" checked required>
                 <label class="custom-control-label" for="credit">IDeal</label>
               </div>
             </div>
@@ -111,7 +111,7 @@
             <div class="row">
               <div class="col-md-6 mb-3">
                 <label for="cc-name">Name on card</label>
-                <input type="text" autocomplete="off" name="cc-name" placeholder="" required>
+                <input type="text" class="form-control" name="cc-name" placeholder="" required>
                 <small class="text-muted">Full name as displayed on card</small>
                 <div class="invalid-feedback">
                   Name on card is required
@@ -121,14 +121,14 @@
             <div class="row">
               <div class="col-md-6 mb-3">
                 <label for="cc-number">IBAN Number</label>
-                <input type="text" autocomplete="off" name="cc-iban" placeholder="" required>
+                <input type="text" class="form-control" name="cc-iban" placeholder="" required>
                 <div class="invalid-feedback">
                   Credit card number is required
                 </div>
               </div>
               <div class="col-md-3 mb-3">
                 <label for="cc-expiration">Card Number</label>
-                <input type="text" autocomplete="off" name="cc-number" placeholder="" required>
+                <input type="text" class="form-control" name="cc-number" placeholder="" required>
                 <div class="invalid-feedback">
                   Expiration date required
                 </div>
