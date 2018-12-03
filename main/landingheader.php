0f0dcb9cffd2a6e518302fd31c270c3ed0a23c16
SonjaVredeveld <sonjavredeveld@gmail.com>
2018-12-03 12:40:30 +0100

    banner width.. again

--- main/landingheader.php
+++ main/landingheader.php
@@ -3,7 +3,7 @@
 			<img src="../img/banner.jpg" id="banner-img">
 			<img src="../img/logo.png" id="logo-img-landing">
 			<h6 id="slogan">
-				<i>"Best slogan ever"</i>
+				<i>"Why go world wide when you can go Wide World?"</i>
 			</h6>
 	</div>
 	<?php require '../search/search.php' ?>
