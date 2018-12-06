ce28588d483097dee6635752a8f7d1c61600c3bd
SonjaVredeveld <sonjavredeveld@gmail.com>
2018-12-06 23:06:50 +0100

    change productpage

--- product/index.php
+++ product/index.php
@@ -15,113 +15,134 @@
 
 <?php require "product.php"; ?>
 <div class="content">
-<div class="outer-slide-container">
-  <div id="myCarousel" class="carousel slide slider-container" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>
+    <div class="outer-slide-container">
+      <div id="myCarousel" class="carousel slide slider-container" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
 
-    </ol>
+        </ol>
 
-    <?php 
-    switch ($categoryID) {
-        case 1:
-            $productimg = "../img/products/Novelty";
-            break;
-        case 2:
-            $productimg = "../img/products/Hoodie";
-            break;
-        case 3:
-            $productimg = "../img/products/Mug";
-            break;
-        case 4:
-            $productimg = "../img/products/T-shirt";
-            break;
-        case 6:
-            $productimg = "../img/products/Novelty";
-            break;
-        case 7:
-            $productimg = "../img/products/Usb";
-            break;
-        case 8:
-            $productimg = "../img/products/Slippers";
-            break;
-        case 9:
-            $productimg = "../img/products/Toy";
-            break;
-        case 10:
-            $productimg = "../img/products/Materials";
-            break;
-    }
-    ?>
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src="<?=$productimg;?>1.jpg" height="400" width="400">
-      </div>
+        <?php 
+        switch ($categoryID) {
+            case 1:
+                $productimg = "../img/products/Novelty";
+                break;
+            case 2:
+                $productimg = "../img/products/Hoodie";
+                break;
+            case 3:
+                $productimg = "../img/products/Mug";
+                break;
+            case 4:
+                $productimg = "../img/products/T-shirt";
+                break;
+            case 6:
+                $productimg = "../img/products/Novelty";
+                break;
+            case 7:
+                $productimg = "../img/products/Usb";
+                break;
+            case 8:
+                $productimg = "../img/products/Slippers";
+                break;
+            case 9:
+                $productimg = "../img/products/Toy";
+                break;
+            case 10:
+                $productimg = "../img/products/Materials";
+                break;
+        }
+        ?>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="<?=$productimg;?>1.jpg" height="400" width="400">
+          </div>
 
-      <div class="item">
-          <img src="<?=$productimg;?>2.jpg" height="400" width="400">
-      </div>
-    
-      <div class="item">
-        <img src="<?=$productimg;?>3.jpg" height="400" width="400">
-      </div>
+          <div class="item">
+              <img src="<?=$productimg;?>2.jpg" height="400" width="400">
+          </div>
+        
+          <div class="item">
+            <img src="<?=$productimg;?>3.jpg" height="400" width="400">
+          </div>
 
-      <div class="item">
-        <a href="https://www.youtube.com/watch?v=l6gz1oQVBkw" target="_blank">
-            <img src="../img/play.jpg" height="400" width="400">
+          <div class="item">
+            <a href="https://www.youtube.com/embed/l6gz1oQVBkw?rel=o&autoplay=1" target="_blank">
+                <img src="../img/play.jpg" height="400" width="400">
+            </a>
+          </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+          <span class="sr-only">Next</span>
         </a>
       </div>
     </div>
 
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-
     <div class="product-description">
             <div class="info row">
                 <p><h3><?=$naam?></h3></p>
+                <table>
+                    
+                
                 <p><a href="../overzicht/productpage.php?category=<?=$categoryID?>&pageNumber=0&sort=0&productAmount=30&filter=0&filterValue=0"><?="Category: " . $categorie?></a>
                     <?php 
                     if($tags[0] != "") {
-                        echo "Tags: ";
                         foreach ($tags as $key) {
-                            echo "<span> $key </span>";
+                            echo "<br><span><i> $key</i></span>";
                         }
+                    } else {
+                        echo "<br>";
                     }
                     ?>
                 </p>
                 <?php if (isset($comments)) {
                     ?>
-                        <p><?="Comments: " . $comments?></p>
+                        <tr>
+                            <td><?="Comments: </td>" . "<td>" . $comments . "</td>"?>
+                        </tr>
                     <?php
-                } ?>
-                <p><?="Made in: " . $herkomst[2]?></p>
-                <p><?="Weight: " . $weight . " kg"?></p>
+                } else {
+                    echo "<br>";
+                }?>
+                    <tr>
+                        <td><?="Made in:</td> " . "<td>" . $herkomst[2] . "</td>"?>
+                    </tr>
+                    <tr>
+                        <td><?="Weight: </td>" . "<td>" . $weight . " kg" . "</td>"?>
+                    </tr>
                 <?php if (isset($color)) {
                     ?>
-                        <p><?="Color: " . $kleur?></p>
+                        <tr>
+                            <td><?="Color: </td>" . "<td>" . $kleur . "</td>"?>
+                        </tr>
                     <?php
-                } ?>
-                <p><?="Packaged: " . $verpakking?></p>
-                <p><?="Price: $ " . $prijs?></p>
+                } else {
+                    echo "<br>";
+                }?>
+                <tr>
+                    <td><?="Packaged: </td>" . "<td>" . $verpakking . "</td>"?>
+                </tr>
+                <tr id="td-price">
+                    <td><?="Price: </td>" . "<td>$" . $prijs . "</td>"?>
+                </tr>
+                </table>
             </div>
-            <form action="../winkelwagen/cart.php?func=add" method="get">
+            <form action="../winkelwagen/cart.php?func=add" method="get" id="order-form">
             <div id="bestelamnt row">
                 <div>
-                    <input type="number" style="margin-left: -15px;" name="aantal" value="1" min="1" max="<?=$voorraad;?>">
+                    <span id="bestelamnt-span">Order now:</span> <input type="number" id="inputnbr" name="aantal" value="1" min="1" max="<?=$voorraad;?>">
                     <?php if($voorraad >= 100) {
                         ?>
                             <p id="available" style="background-color: green; color: white;">
@@ -153,11 +174,13 @@
             </button>
         </form>
     </div>
-    </div>
+</div>
 
     <div>
-        <div class="recommended col-md-11" style="margin-top: -200px; margin-left: 100px; margin-bottom: 50px;">
-        <h2>Perhaps you are also interested in:</h2>
+        <div style="width: 100%; margin-top: 150px">        
+            <h2>Perhaps you are also interested in:</h2>
+        </div>
+        <div class="recommended">
             <?php 
             $i=0;
             $recommended = [];
@@ -173,11 +196,11 @@
             for ($i=0; $i < 3; $i++) { 
                 $imgindex = rand(1,3);
                 ?>
-                <div class="col-md-4">
+                <div class="recommend-div">
                     <a href="../product/index.php?product=<?=$recommended[$i]['id'];?>&category=<?=$categoryID;?>">
                         <img src='<?php echo $productimg . $imgindex;?>.jpg' alt='Productimg' heigt='200px' width='200px'>
                         <p><?=$recommended[$i]['naam'];?></p>
-                        <p><?=$recommended[$i]['prijs'];?></p>
+                        <p>$<?=$recommended[$i]['prijs'];?></p>
                     </a>
                 </div>
                 <?php
